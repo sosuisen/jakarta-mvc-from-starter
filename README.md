0175aa9f3b1bc82000b283670b0e73dd69364b02
sosuisen <hidekazu.kubota@gmail.com>
2024-07-23 17:56:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Jakarta MVC Tutorial for Beginners (1)
+
+This is the first part of the Jakarta MVC Tutorial for Beginners.
+In this part, we will create a Jakarta MVC project using Payara Starter.
+
+### Prerequisites for This Tutorial
+
+- Java SE 21
+- Windows 10+
+
+## Running the Application
+
+```
+.\mvnw package payara-micro:dev
+```
---
+++ src/main/java/com/example/hello/HelloWorldResource.java
@@ -0,0 +1,26 @@
+package com.example.hello;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.mvc.Controller;
+import jakarta.mvc.Models;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+@Controller
+@RequestScoped
+@Path("hello")
+public class HelloWorldResource {
+    @Inject
+    private Models models;
+
+    @GET
+    public String hello(@QueryParam("name") String name) {
+        if ((name == null) || name.trim().isEmpty()) {
+            name = "Jakarta EE";
+        }
+        models.put("name", name);
+        return "hello.html";
+    }
+}
